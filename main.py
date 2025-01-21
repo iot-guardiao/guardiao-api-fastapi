3ed594123444b52253cf7325bb776b4fede99d14
Your Name <you@example.com>
2025-01-21 16:43:17 -0300

    add verificação para identificar um mesmo agendamento com os mesmos dados

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from api.v1.agendamento.controller import router as agendamento_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(agendamento_router)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
